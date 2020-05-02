8905c1a6a230eb5b7a7b3b3f3c95b7b7915538b9
Daniel Villarroel <handonsk82894@gmail.com>
2020-05-01 18:11:11 -0700

    Created event listener for all saveBtn that saves the user event and calls renderLastRegistered

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,29 @@
 //invoking moment.js and displaying it in the header of the planner
-const presentTime = moment();
+const presentTime = moment().format("LLL");
 const currentDayEl = document.querySelector("#currentDay");
 
 currentDayEl.textContent = presentTime;
+
+const textAreaInputEl = document.querySelector(".eventTextArea");
+const userEventText = document.querySelector("#userEventText");
+// renders the user event in the text area
+renderLastRegistered();
+
+function renderLastRegistered() {
+  const textAreaInput = localStorage.getItem("userEvent");
+  if (textAreaInput === null) {
+    return;
+  } else {
+    textAreaInputEl.textContent = textAreaInput;
+  }
+}
+// Created event listener for all saveBtn that saves the user event and calls renderLastRegistered
+$(".saveBtn").on("click", function () {
+  const textAreaInput = document.querySelector(".eventTextArea").value;
+  if (textAreaInput === "") {
+    alert("please provide an event in order to save");
+  } else {
+    localStorage.setItem("userEvent", textAreaInput);
+    renderLastRegistered();
+  }
+});
